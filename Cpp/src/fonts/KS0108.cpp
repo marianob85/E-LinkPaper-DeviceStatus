a94beebb3ea2dfb917229d6a6a1f880d85edb19a
Mariano <marianob85work@gmail.com>
2018-05-29 22:50:04 +0200

    Modified   Cpp/src/fonts/KS0108.cpp

--- Cpp/src/fonts/KS0108.cpp
+++ Cpp/src/fonts/KS0108.cpp
@@ -17,20 +17,11 @@ FontPainter::Size FontPainterKS0108::drawChar( unsigned startX, unsigned startY,
 	const auto startByte	 = ( chacterIndex * ( heightBytes * 8 * m_fontData.width + 8 ) ) / 8;
 	const CharData* charData = reinterpret_cast< const CharData* >( m_fontData.chars + startByte );
 
-	// Uncolored (0) -> czarne
-
 	const unsigned short* dataPtr = charData->data;
-	for( size_t x = 0; x < charData->width; ++x )
-	{
+	for( size_t x = 0; x < charData->width; ++x, dataPtr += heightBytes )
 		for( size_t y = 0; y < m_fontData.height; ++y )
-		{
-			const bool pixel = ( 0x01 & ( *( dataPtr + ( y / 8 ) ) >> y % 8 ) ) > 0;
-			if( pixel )
+			if( 0x01 & ( *( dataPtr + ( y / 8 ) ) >> y % 8 ) )
 				m_drawPixel( startX + x, startY + y, !color );
-		}
-
-		dataPtr += heightBytes;
-	}
 
 	return { charData->width, m_fontData.height };
 }
