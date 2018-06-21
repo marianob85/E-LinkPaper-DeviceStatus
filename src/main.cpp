e64b03d1a0a902b086649426ce9f7f81ff8210d4
Mariusz Brzeski <marianob85@gmail.com>
2018-06-21 22:14:01 +0200

    Modified   configure.ac Modified   msvc/E-LinkStatus.vcxproj Modified   msvc/E-LinkStatus.vcxproj.filters Modified   src/main.cpp Add        src/lib/wiringPi/version.h Add        src/lib/wiringPi/wiringPi/ads1115.c Add        src/lib/wiringPi/wiringPi/ads1115.h Add        src/lib/wiringPi/wiringPi/bmp180.c Add        src/lib/wiringPi/wiringPi/bmp180.h Add        src/lib/wiringPi/wiringPi/drcNet.c Add        src/lib/wiringPi/wiringPi/drcNet.h Add        src/lib/wiringPi/wiringPi/drcSerial.c Add        src/lib/wiringPi/wiringPi/drcSerial.h Add        src/lib/wiringPi/wiringPi/ds18b20.c Add        src/lib/wiringPi/wiringPi/ds18b20.h Add        src/lib/wiringPi/wiringPi/htu21d.c Add        src/lib/wiringPi/wiringPi/htu21d.h Add        src/lib/wiringPi/wiringPi/max31855.c Add        src/lib/wiringPi/wiringPi/max31855.h Add        src/lib/wiringPi/wiringPi/max5322.c Add        src/lib/wiringPi/wiringPi/max5322.h Add        src/lib/wiringPi/wiringPi/mcp23008.c Add        src/lib/wiringPi/wiringPi/mcp23008.h Add        src/lib/wiringPi/wiringPi/mcp23016.c Add        src/lib/wiringPi/wiringPi/mcp23016.h Add        src/lib/wiringPi/wiringPi/mcp23016reg.h Add        src/lib/wiringPi/wiringPi/mcp23017.c Add        src/lib/wiringPi/wiringPi/mcp23017.h Add        src/lib/wiringPi/wiringPi/mcp23s08.c Add        src/lib/wiringPi/wiringPi/mcp23s08.h Add        src/lib/wiringPi/wiringPi/mcp23s17.c Add        src/lib/wiringPi/wiringPi/mcp23s17.h Add        src/lib/wiringPi/wiringPi/mcp23x08.h Add        src/lib/wiringPi/wiringPi/mcp23x0817.h Add        src/lib/wiringPi/wiringPi/mcp3002.c Add        src/lib/wiringPi/wiringPi/mcp3002.h Add        src/lib/wiringPi/wiringPi/mcp3004.c Add        src/lib/wiringPi/wiringPi/mcp3004.h Add        src/lib/wiringPi/wiringPi/mcp3422.c Add        src/lib/wiringPi/wiringPi/mcp3422.h Add        src/lib/wiringPi/wiringPi/mcp4802.c Add        src/lib/wiringPi/wiringPi/mcp4802.h Add        src/lib/wiringPi/wiringPi/pcf8574.c Add        src/lib/wiringPi/wiringPi/pcf8574.h Add        src/lib/wiringPi/wiringPi/pcf8591.c Add        src/lib/wiringPi/wiringPi/pcf8591.h Add        src/lib/wiringPi/wiringPi/piHiPri.c Add        src/lib/wiringPi/wiringPi/piThread.c Add        src/lib/wiringPi/wiringPi/pseudoPins.c Add        src/lib/wiringPi/wiringPi/pseudoPins.h Add        src/lib/wiringPi/wiringPi/rht03.c Add        src/lib/wiringPi/wiringPi/rht03.h Add        src/lib/wiringPi/wiringPi/sn3218.c Add        src/lib/wiringPi/wiringPi/sn3218.h Add        src/lib/wiringPi/wiringPi/softPwm.c Add        src/lib/wiringPi/wiringPi/softPwm.h Add        src/lib/wiringPi/wiringPi/softServo.c Add        src/lib/wiringPi/wiringPi/softServo.h Add        src/lib/wiringPi/wiringPi/softTone.c Add        src/lib/wiringPi/wiringPi/softTone.h Add        src/lib/wiringPi/wiringPi/sr595.c Add        src/lib/wiringPi/wiringPi/sr595.h Add        src/lib/wiringPi/wiringPi/wiringPi.c Add        src/lib/wiringPi/wiringPi/wiringPi.h Add        src/lib/wiringPi/wiringPi/wiringPiI2C.c Add        src/lib/wiringPi/wiringPi/wiringPiI2C.h Add        src/lib/wiringPi/wiringPi/wiringPiSPI.c Add        src/lib/wiringPi/wiringPi/wiringPiSPI.h Add        src/lib/wiringPi/wiringPi/wiringSerial.c Add        src/lib/wiringPi/wiringPi/wiringSerial.h Add        src/lib/wiringPi/wiringPi/wiringShift.c Add        src/lib/wiringPi/wiringPi/wiringShift.h Add        src/lib/wiringPi/wiringPi/wpiExtensions.c Add        src/lib/wiringPi/wiringPi/wpiExtensions.h Add        src/lib/wiringPi/wiringPiD/daemonise.c Add        src/lib/wiringPi/wiringPiD/daemonise.h Add        src/lib/wiringPi/wiringPiD/drcNetCmd.h Add        src/lib/wiringPi/wiringPiD/network.c Add        src/lib/wiringPi/wiringPiD/network.h Add        src/lib/wiringPi/wiringPiD/runRemote.c Add        src/lib/wiringPi/wiringPiD/runRemote.h Add        src/lib/wiringPi/wiringPiD/wiringpid.c

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 // start: ------------------- Test -------------------
 #include <epdWiringPi.hpp>
 #include <epd7in5b.hpp>
+#include <wiringPiI2C.h>
 // end: --------------------- Test -------------------
 
 using namespace std;
@@ -35,9 +36,54 @@ void sig_handler( int sig )
 		abort();
 	}
 }
+/*=========================================================================
+	I2C ADDRESS/BITS
+	-----------------------------------------------------------------------*/
+#define SI7021_DEFAULT_ADDRESS ( 0x40 )
+
+#define SI7021_MEASRH_HOLD_CMD 0xE5
+#define SI7021_MEASRH_NOHOLD_CMD 0xF5
+#define SI7021_MEASTEMP_HOLD_CMD 0xE3
+#define SI7021_MEASTEMP_NOHOLD_CMD 0xF3
+#define SI7021_READPREVTEMP_CMD 0xE0
+#define SI7021_RESET_CMD 0xFE
+#define SI7021_WRITERHT_REG_CMD 0xE6
+#define SI7021_READRHT_REG_CMD 0xE7
+#define SI7021_WRITEHEATER_REG_CMD 0x51
+#define SI7021_READHEATER_REG_CMD 0x11
+#define SI7021_ID1_CMD 0xFA0F
+#define SI7021_ID2_CMD 0xFCC9
+#define SI7021_FIRMVERS_CMD 0x84B8
+
+void test()
+{
+	auto fd  = wiringPiI2CSetup( 0x40 );
+	auto ret = wiringPiI2CWrite( fd, SI7021_RESET_CMD );
+	//ret		 = wiringPiI2CWrite( fd, SI7021_MEASTEMP_HOLD_CMD );
+	//std::this_thread::sleep_for( std::chrono::milliseconds( 300 ) );
+	//unsigned int a = wiringPiI2CRead( fd );
+	//std::cout << a << endl;
+	//unsigned int b = wiringPiI2CRead( fd );
+	//std::cout << b << endl;
+	//unsigned int c = wiringPiI2CRead( fd );
+	//std::cout << c << endl;
+
+	auto temp = wiringPiI2CReadReg8( fd, SI7021_MEASTEMP_HOLD_CMD );
+
+	//unsigned int temp = a;
+	//temp <<= 8;
+	//temp |= b;
+
+	float temperature = temp;
+	temperature *= 175.72;
+	temperature /= 65536;
+	temperature -= 46.85;
+}
 
 int main( int argc, char** argv )
 {
+	test();
+
 	path xmlPath( "usr/local/etc/E-LinkStatusConfig.xml" );
 
 	if( argc > 1 )
