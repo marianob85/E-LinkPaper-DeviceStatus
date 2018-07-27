139a703d3bc70f09da7d6fcea6277a38ea21b2af
Mariusz Brzeski <marianob85@gmail.com>
2018-07-27 20:45:21 +0200

    modified:   arm-linux-gnueabihf.cmake

--- CMake/arm-linux-gnueabihf.cmake
+++ CMake/arm-linux-gnueabihf.cmake
@@ -10,3 +10,5 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_compile_options(-Wno-psabi)
